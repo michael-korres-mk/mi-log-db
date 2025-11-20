b36c45c58cebc5a5becbf00b884513d1cffa6c48
mkorres <mkorres@iknowhow.com>
2025-11-20 10:15:02 +0200

    --- timestamp regex & formatting

--- back-end/src/main/java/com/mikorsoft/milogdb/service/DataImportServiceImpl.java
+++ back-end/src/main/java/com/mikorsoft/milogdb/service/DataImportServiceImpl.java
@@ -5,6 +5,9 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,24 +18,21 @@ public class DataImportServiceImpl implements DataImportService {
 //	143.127.131.4 - - [09/Jun/2005:23:48:54 -0400] "GET / HTTP/1.1" 403 3931 "-" "Mozilla/4.0 (compatible; MSIE 5.5; Windows 98)"
 
 	private final String IP_REGEX = "(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})";
-
-//	[09/Jun/2005:07:11:21 -0400]
+	private final String TIMESTAMP_REGEX = "\\[(\\d{2}/[A-Za-z]{3}/\\d{4}:\\d{2}:\\d{2}:\\d{2} -\\d{4})]";
 
 	private final String ANY_CHARACTER = ".*";
 
+	private final String accessLogRegex = "^" + IP_REGEX + " - - " + TIMESTAMP_REGEX +
+
+
+//				"\"([^\"]*)\"" + " (\\d{3}) " + "(\\d+|-) " +
+//				"\"([^\"]*)\" \"([^\"]*)\"$" +
+			ANY_CHARACTER
+			;
 
 	public void importFile(String filename,String regex) throws IOException {
 
 		System.out.println("IMPORT FILE !");
-
-		String accessLogRegex = "^" + IP_REGEX + " - - " +
-
-
-//				"\\[(\\d{1,2}/[A-Za-z]{3}/\\d{4}:\\d{1,2}:\\d{1,2}:\\d{1,2} -\\d{4})]" +
-//				"\"([^\"]*)\"" + " (\\d{3}) " + "(\\d+|-) " +
-//				"\"([^\"]*)\" \"([^\"]*)\"$" +
-				ANY_CHARACTER
-				;
 
 
 		File file = new ClassPathResource(filename).getFile();
@@ -42,17 +42,27 @@ public class DataImportServiceImpl implements DataImportService {
 			String line;
 			int n = 10;
 			while ((n-- > 0) && !(line = is.readLine()).isEmpty()) {
-//				System.out.println(n + ". " + line);
-
 				Matcher m = p.matcher(line);
 
-				if (m.matches()) {
-					String IP = m.group(1);
-					System.out.println("date = " + IP);
+				this.createAccessLogRecord(m);
 
-//					String time = m.group(2);
-//					System.out.println("time = " + time);
-//
+			}
+		}
+
+	}
+
+	private void createAccessLogRecord(Matcher m) {
+		if (m.matches()) {
+			String IP = m.group(1);
+			System.out.println("IP = " + IP);
+
+			String timestamp = m.group(2);
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MMM/yyyy:HH:mm:ss Z", Locale.ENGLISH);
+			ZonedDateTime zdt = ZonedDateTime.parse(timestamp, formatter);
+			System.out.println("timestamp = " + zdt);
+
+
+
 //					String threadId = m.group(3);
 //					System.out.println("threadId = " + threadId);
 //
@@ -73,13 +83,7 @@ public class DataImportServiceImpl implements DataImportService {
 
 //					new AccessLogRecord(date,time,threadId,level,component,blockId,src,dest);
 
-				}
-
-			}
 		}
-
-
-
-		}
+	}
 
 }
