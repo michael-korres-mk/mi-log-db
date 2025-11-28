93def5566741bce4cf4d787f3d2949889c2cd517
mkorres <mkorres@iknowhow.com>
2025-11-28 14:25:52 +0200

    --- principal name rendering --- AuthController cleanup

--- src/main/java/com/mikorsoft/milogdb/ui/AuthController.java
+++ src/main/java/com/mikorsoft/milogdb/ui/AuthController.java
@@ -3,6 +3,7 @@ package com.mikorsoft.milogdb.ui;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -10,18 +11,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AuthController {
 
 	@GetMapping("/")
-	public String auth(@RequestParam(required = false) Integer mode, Model model) {
-
-		System.out.println("Selected mode: " + ((mode == null)? 0 : mode)); // 0 or 1
-		model.addAttribute("mode", (mode == null)? 0 : mode);
-
+	public String auth(@RequestParam(required = false, defaultValue = "0") Integer mode, Model model) {
+		System.out.println("Selected mode: " + mode); // 0 or 1
+		model.addAttribute("mode", mode);
 		model.addAttribute("pageTitle", "MiLogDB");
 		model.addAttribute("headerTitle", "Welcome to MiLogDB");
 
 		return "auth";
 	}
 
-	@GetMapping("/users")
+	@PostMapping("/signup")
 	public String login(@RequestParam String username,@RequestParam String password, Model model) {
 
 		System.out.println(username);
--- src/main/java/com/mikorsoft/milogdb/ui/HomeController.java
+++ src/main/java/com/mikorsoft/milogdb/ui/HomeController.java
@@ -4,12 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
+
 @Controller
 public class HomeController {
 
 	@GetMapping("/home")
-	public String home(Model model) {
-		model.addAttribute("name", "Student");
+	public String home(Model model, Principal principal) {
+		model.addAttribute("name", principal.getName());
 		model.addAttribute("pageTitle", "MiLogDB");
 		model.addAttribute("headerTitle", "Welcome to MiLogDB");
 
